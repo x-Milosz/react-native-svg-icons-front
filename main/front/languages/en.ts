5b4fc2bcb0e290246a2807034987f8a0860991e8
Miłosz Nowaczyk <milosz.nowaczyk@protonmail.com>
2023-03-27 23:27:10 +0200

    style: change names

--- main/front/languages/en.ts
+++ main/front/languages/en.ts
@@ -10,7 +10,7 @@ const en = {
 
     },
     "mainPage": {
-        title: "React native svg icons",
+        title: "React Native Svg Icons",
         pageTitle: "RNSI :: Home",
         homeButtonText: "HOME",
         aboutProjectButtonText: "ABOUT PROJECT",
@@ -23,12 +23,12 @@ const en = {
     },
     "aboutProjectPage": {
         pageTitle: "RNSI :: About project",
-        title: "React native svg icons",
+        title: "React Native Svg Icons",
         description: "Description",
         text1: "React Native Svg Icons is a free software project that aims to provide React Native developers with icons for their projects in an easy and convenient way, without the need to install additional dependencies and libraries. The project uses icons from Material Design, which ensures a consistent look and style in React Native applications.",
         text2: "The project consists of two parts: a frontend written in NextJs and Typescript, which is responsible for converting SVGs into React components, and a backend written in SpringBoot using Kotlin, which handles requests related to SVGs.",
-        text3: "Using React Native SVG Icons is simple and intuitive. Developers can simply import the appropriate icon component into their project and use it in their code. The components returned by the website are susceptible to modifications and stylistic changes, allowing icons to be customized to the individual needs of the project.",
-        text4: "Additional features and options are planned for the future, which will further facilitate the use of React Native SVG Icons. This will give developers even greater control over icons in their projects and enable them to tailor them to their needs.",
+        text3: "Using React Native Svg Icons is simple and intuitive. Developers can simply import the appropriate icon component into their project and use it in their code. The components returned by the website are susceptible to modifications and stylistic changes, allowing icons to be customized to the individual needs of the project.",
+        text4: "Additional features and options are planned for the future, which will further facilitate the use of React Native Svg Icons. This will give developers even greater control over icons in their projects and enable them to tailor them to their needs.",
         techStack: "Tech stack",
         techStackText: "Project contains of:",
         techStackList1: "NextJs, Typescript - frontend, svg conversion",
