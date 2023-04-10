62a608f740637122c47b6f51041b61ad60acf36d
Miłosz Nowaczyk <milosz.nowaczyk@protonmail.com>
2023-04-10 23:58:12 +0200

    fix: api

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const isDev = process.env.NODE_ENV === "development";
 const devServerAddress = "http://localhost:3001/api/";
-const prodServerAddress = "https://api.react-native.svg-icons.miłosznowaczyk.name/api/";
+const prodServerAddress = "https://api.react-native-svg-icons.miłosznowaczyk.name/api/";
 export const serverAddress = isDev ? devServerAddress : prodServerAddress ;
